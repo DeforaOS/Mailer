0f369521277b47144d21d2250e8464aed95c4ffb
Pierre Pronchery <khorben@defora.org>
2018-02-12 12:57:54 +0100

    Switch to the 2-clause BSD license

--- tests/pkgconfig.sh
+++ tests/pkgconfig.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #$Id$
 #Copyright (c) 2016 Pierre Pronchery <khorben@defora.org>
-#This file is part of DeforaOS Desktop libDesktop
+#This file is part of DeforaOS Desktop Mailer
 #Redistribution and use in source and binary forms, with or without
 #modification, are permitted provided that the following conditions are met:
 #
