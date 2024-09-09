abee839f11addbff319272b40894cfa164f60949
Pierre Pronchery <khorben@defora.org>
2024-09-09 21:12:57 +0200

    mailer: avoid a build issue with modern compilers

--- src/account/imap4.c
+++ src/account/imap4.c
@@ -190,7 +190,7 @@ AccountConfig const _imap4_config[I4CV_COUNT + 1] =
 	{ "username",	"Username",		ACT_STRING,	NULL	},
 	{ "password",	"Password",		ACT_PASSWORD,	NULL	},
 	{ "hostname",	"Server hostname",	ACT_STRING,	NULL	},
-	{ "port",	"Server port",		ACT_UINT16,	143	},
+	{ "port",	"Server port",		ACT_UINT16,	(void *)143 },
 	{ "ssl",	"Use SSL",		ACT_BOOLEAN,	0	},
 #if 0 /* XXX not implemented yet */
 	{ "sent",	"Sent mails folder",	ACT_NONE,	NULL	},
--- src/account/nntp.c
+++ src/account/nntp.c
@@ -41,7 +41,7 @@ static AccountConfig const _nntp_config[] =
 	{ "username",	"Username",		ACT_STRING,	NULL	},
 	{ "password",	"Password",		ACT_PASSWORD,	NULL	},
 	{ "hostname",	"Server hostname",	ACT_STRING,	NULL	},
-	{ "port",	"Server port",		ACT_UINT16,	119	},
+	{ "port",	"Server port",		ACT_UINT16,	(void *)119 },
 	{ "ssl",	"Use SSL",		ACT_BOOLEAN,	0	},
 	{ NULL,		NULL,			ACT_NONE,	NULL	}
 };
